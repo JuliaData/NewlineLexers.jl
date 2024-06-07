7f921ead66341d6160311fe5d653aa1bafc29486
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2024-06-07 16:47:59 +0200

    const _AVOID_PLATFORM_SPECIFIC_LLVM_CODE = false

--- src/NewlineLexers.jl
+++ src/NewlineLexers.jl
@@ -18,7 +18,7 @@ end
 # happens with PackageCompiler and only when targetting multiple cpu targets.
 # TODO: use Preferences.jl and more granular feature settings
 # const _AVOID_PLATFORM_SPECIFIC_LLVM_CODE = get(ENV, "JULIA_CPU_TARGET", "-Cnative" in Base.julia_cmd().exec ? "native" : "") != "native"
-const _AVOID_PLATFORM_SPECIFIC_LLVM_CODE = get(ENV, "NEWLINELEXERS_NATIVE", "false") == "false"
+const _AVOID_PLATFORM_SPECIFIC_LLVM_CODE = false # get(ENV, "NEWLINELEXERS_NATIVE", "false") == "false"
 
 # Compare two vectors of 64 bytes and produce an UInt64 where the set bits
 # indicate the positions where the two vectors match.
